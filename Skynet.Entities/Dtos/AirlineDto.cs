d24e3d724ed638d0f8547d85ca961ab74872ccea
Dani Rashba <danirdd92@gmail.com>
2020-10-04 02:45:58 +0300

    Added sample Dtos

---
+++ Skynet.Entities/Dtos/AirlineDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Skynet.Entities.Dtos
+{
+    public class AirlineDto
+    {
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public int CountryId { get; set; }
+    }
+
+
+}
---
+++ Skynet.Entities/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skynet.Entities.Dtos
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+
+}
