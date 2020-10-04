21017322bc793d4250a3e41781a613cc4c288db3
Dani Rashba <danirdd92@gmail.com>
2020-10-04 15:11:19 +0300

    Added testing controllers

---
+++ Skynet.Api/Controllers/AirlineController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Skynet.Entities.Dtos;
+using Skynet.Repository;
+
+namespace Skynet.Api.Controllers
+{
+    [Route("api/airlines")]
+    [ApiController]
+    public class AirlineController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly IMapper _mapper;
+
+        public AirlineController(IRepositoryManager repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var airlines = _repository.Airline.GetAllAirlines();
+            var airlinesDto = _mapper.Map<IEnumerable<AirlineDto>>(airlines);
+            return Ok(airlinesDto);
+        }
+    }
+}
---
+++ Skynet.Api/Controllers/CountryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Skynet.Entities.Dtos;
+using Skynet.Entities.Models;
+using Skynet.Repository;
+
+namespace Skynet.Api.Controllers
+{
+    [Route("api/countries")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly IMapper _mapper;
+
+        public CountryController(IRepositoryManager repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCountries()
+        {
+            var countries = _repository.Country.GetAllCountries();
+            var countriesDto = _mapper.Map<IEnumerable<CountryDto>>(countries);
+            return Ok(countriesDto);
+        }
+    }
+}
